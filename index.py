f56585a635c506fee1f24d6f0fa2f2a74f05aaa5
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-12-07 18:26:37 +0700

    feat(forecast): Add future time

--- index.py
+++ index.py
@@ -24,72 +24,100 @@ DATA_FROM = 2024
 @app.route('/predict', methods=['POST'])
 def predict():  
     data = request.get_json()
-    # Get stock and steps from request
-    stock = str(data['stock']).upper()
-    steps = int(data['steps'])
+    if not data or "stock" not in data or "steps" not in data:
+        return jsonify({
+            "status": "failed",
+            "error": "Missing risk profile!"
+        }),400
 
-    # Current Year
-    current_year = int(datetime.now().year)
-    '''
-    Kalo request dari tahun 2025 dengan steps 5, maka akan prediksi s/d 2023
-    Karena data sampai dengan tahun 2024, maka kita harus menambahkan 1 (2025 - 2024)
-    '''
-    gap = current_year - DATA_FROM
-    steps += gap
+    try:
+        # Get stock and steps from request
+        stock = str(data['stock']).upper()
+        steps = int(data['steps'])
 
-    # ===== UNCOMMENT UNTUK DOWNLOAD DARI GCS =====
-    # model_path = f"stock_model/{stock}/model_saham.h5"
-    # scaler_path = f"stock_model/{stock}/scaler.pkl"
-    # csv_path = f"stock_model/{stock}/data_saham.csv"
-    #
-    # local_model_path = f"models/{stock}"
-    # local_scaler_path = f"scalers/{stock}"
-    # local_csv_path = f"csv/{stock}"
+        # Current Year
+        current_year = int(datetime.now().year)
+        '''
+        Kalo request dari tahun 2025 dengan steps 5, maka akan prediksi s/d 2023
+        Karena data sampai dengan tahun 2024, maka kita harus menambahkan 1 (2025 - 2024)
+        '''
+        gap = current_year - DATA_FROM
+        steps += gap
 
-    # Check dirs
-    # if not os.path.exists(local_model_path):
-    #     os.makedirs(local_model_path)
-    # if not os.path.exists(local_scaler_path):
-    #     os.makedirs(local_scaler_path)
-    # if not os.path.exists(local_csv_path):
-    #     os.makedirs(local_csv_path)
+        # ===== UNCOMMENT UNTUK DOWNLOAD DARI GCS =====
+        # model_path = f"stock_model/{stock}/model_saham.h5"
+        # scaler_path = f"stock_model/{stock}/scaler.pkl"
+        # csv_path = f"stock_model/{stock}/data_saham.csv"
+        #
+        # local_model_path = f"models/{stock}"
+        # local_scaler_path = f"scalers/{stock}"
+        # local_csv_path = f"csv/{stock}"
 
-    # model = load_model_from_gcs(BUCKET_NAME, model_path, local_model_path + "/model_saham.h5")
-    # scaler = load_scaler_from_gcs(BUCKET_NAME, scaler_path, local_scaler_path + "/scaler.pkl")
-    # load_csv_from_gcs(BUCKET_NAME, csv_path, local_csv_path + "/data_saham.csv")
+        # Check dirs
+        # if not os.path.exists(local_model_path):
+        #     os.makedirs(local_model_path)
+        # if not os.path.exists(local_scaler_path):
+        #     os.makedirs(local_scaler_path)
+        # if not os.path.exists(local_csv_path):
+        #     os.makedirs(local_csv_path)
 
-    # model, scaler = load_from_gcs(BUCKET_NAME, paths={
-    #     "model_path": model_path,
-    #     "local_model_path": local_model_path + "/model_saham.h5",
-    #     "scaler_path": scaler_path,
-    #     "local_scaler_path": local_scaler_path +"/scaler.pkl",
-    #     "csv_path": csv_path,
-    #     "local_csv_path": local_csv_path + "/data_saham.csv"
+        # model = load_model_from_gcs(BUCKET_NAME, model_path, local_model_path + "/model_saham.h5")
+        # scaler = load_scaler_from_gcs(BUCKET_NAME, scaler_path, local_scaler_path + "/scaler.pkl")
+        # load_csv_from_gcs(BUCKET_NAME, csv_path, local_csv_path + "/data_saham.csv")
+
+        # model, scaler = load_from_gcs(BUCKET_NAME, paths={
+        #     "model_path": model_path,
+        #     "local_model_path": local_model_path + "/model_saham.h5",
+        #     "scaler_path": scaler_path,
+        #     "local_scaler_path": local_scaler_path +"/scaler.pkl",
+        #     "csv_path": csv_path,
+        #     "local_csv_path": local_csv_path + "/data_saham.csv"
+        # })
+        # ===== UNCOMMENT UNTUK DOWNLOAD DARI GCS =====
+
+        # Load model and scaler based on the stock
+        model = tf.keras.models.load_model(f"models/{stock}/model_saham.h5")
+        scaler = joblib.load(f"scalers/{stock}/scaler.pkl")
+
+        # Get the data
+        # TIME, SERIES = parse_data_from_file(local_csv_path + "/data_saham.csv")
+        TIME, SERIES = parse_data_from_file(f"csv/{stock}/data_saham.csv")
+        SERIES = scaler.fit_transform(SERIES.reshape(-1, 1)).flatten()
+
+        # Generate Future Times
+        future_time  = pd.date_range(start=TIME[-1], periods=52 * steps + 1, freq='W')[1:]
+
+        # Predict
+        predicted_values = extended_forecast(model, SERIES, WINDOW_SIZE, forecast_steps=steps)
+
+        # Convert to actual price
+        predicted_actual = scaler.inverse_transform([predicted_values]).flatten().tolist()
+
+        # Response
+        response = {
+            'status': 'success',
+            'prediction': {
+                'prices': predicted_actual,
+                'times': future_time.tolist()
+            },
+            'year_from': str(DATA_FROM),
+            'year_to': str(DATA_FROM + steps),
+        }
+
+        return jsonify(response), 200
+    except Exception as e:
+        return jsonify({
+            "status": "failed",
+            "error": str(e)
+        }),400
+
+    # # Return json (array)
+    # return jsonify({
+    #     'predictions': predicted_actual.flatten().tolist(),
+    #     'year_from': str(DATA_FROM),
+    #     'year_to': str(DATA_FROM + steps),
+    #     'prediction': predicted_actual.flatten().tolist()[-1]
     # })
-    # ===== UNCOMMENT UNTUK DOWNLOAD DARI GCS =====
-
-    # Load model and scaler based on the stock
-    model = tf.keras.models.load_model(f"models/{stock}/model_saham.h5")
-    scaler = joblib.load(f"scalers/{stock}/scaler.pkl")
-
-    # Get the data
-    # TIME, SERIES = parse_data_from_file(local_csv_path + "/data_saham.csv")
-    TIME, SERIES = parse_data_from_file(f"csv/{stock}/data_saham.csv")
-    SERIES = scaler.fit_transform(SERIES.reshape(-1, 1)).flatten()
-
-    # Predict
-    predicted_values = extended_forecast(model, SERIES, WINDOW_SIZE, forecast_steps=steps)
-
-    # Convert to actual price
-    predicted_actual = scaler.inverse_transform([predicted_values])
-
-    # Return json (array)
-    return jsonify({
-        'predictions': predicted_actual.flatten().tolist(),
-        'year_from': str(DATA_FROM),
-        'year_to': str(DATA_FROM + steps),
-        'prediction': predicted_actual.flatten().tolist()[-1]
-    })
     
 @app.route('/riskprofile', methods=['POST'])
 def riskProfile():
